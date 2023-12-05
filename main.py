f17b3697cab9ed3b94b0e48fe145ef74faa11852
antohachekhov <bortnikovnik@gmail.com>
2023-12-06 00:55:57 +0700

    Разработан модуль запуска и настройки сервера

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from photoAlbumServer import photoAlbumServer
-import os
-import threading
-import serial
 import serial.tools.list_ports
-from clientTest import client
+
+defaultSettings = {
+    'pathToPhotoBase': 'photoBase',
+    'portName': 'COM1',
+    'baudrate': 9600
+}
 
 if __name__ == "__main__":
-    s = photoAlbumServer()
-    thr1 = threading.Thread(target=s.start)
-    thr1.start()
-    print('Север создан')
+    print('======= ПРОГРАММА-СЕРВЕР ПО РАБОТЕ С ФОТОГРАФИЯМИ =======')
+    print('--- Настройки сервера по умолчанию ---')
+    for setting in defaultSettings:
+        print(f'{setting}: {defaultSettings[setting]}')
+    flag = input('Применить настройки по умолчанию? да/нет: ')
+    if flag.lower() == 'нет':
+        defaultSettings['pathToPhotoBase'] = input('Введите путь к корневой папке фотобазы: ')
+        ports = serial.tools.list_ports.comports()
+        portsList = [port.device for port in ports]
+        print('Доступные следующие COM-порты:', *portsList)
+        defaultSettings['portName'] = input('Введите название COM-порта: ')
+        defaultSettings['baudrate'] = int(input('Введите скорость передачи данных в бит/сек: '))
 
-    thr2 = threading.Thread(target=client)
-    thr2.start()
+    s = photoAlbumServer(inputPathToPhotoBase=defaultSettings['pathToPhotoBase'])
+    s.start(portName=defaultSettings['portName'], baudrate=defaultSettings['baudrate'])
